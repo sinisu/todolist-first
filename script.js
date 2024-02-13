4ec9411c21d365cee17a0d157c0fcc8061081079
yisoo song <yisoo1049@gmail.com>
2024-02-14 02:37:48 +0900

    second commit

--- script.js
+++ script.js
@@ -5,8 +5,12 @@ let todolist=[]
 addButton.addEventListener("click",addlist)
 
 function addlist(){
-    let taskContent=userInput.value
-    todolist.push(taskContent);
+    let taskItem={
+        id:randomIDGenerate(),
+        taskContent: userInput.value,
+        isComplete:false,
+    }
+    todolist.push(taskItem);
     console.log(todolist);
     render();
 }
@@ -14,16 +18,43 @@ function addlist(){
 function render(){
     let resultHTML="";
     for(let i=0;i<todolist.length;i++){
-        resultHTML +=`<div class="task-list">
-        <div>${todolist[i]}</div>
+        if(todolist[i].isComplete==true){
+            resultHTML +=`<div class="task-list">
+        <div class="task-done">${todolist[i].taskContent}</div>
         <div>
-            <button>check</button>
+            <button onclick="toggleComplete('${todolist[i].id}')">check</button>
+            <button>delete</button>
+        </div>
+    </div>`
+        } else{
+            resultHTML +=`<div class="task-list">
+        <div>${todolist[i].taskContent}</div>
+        <div>
+            <button onclick="toggleComplete('${todolist[i].id}')">check</button>
             <button>delete</button>
         </div>
     </div>`;
+        }
+
+        
 
     }
 
     document.getElementById("task-content").innerHTML=resultHTML;
 }
 
+function toggleComplete(id){
+    console.log("id:",id)
+    for(let i=0;i<todolist.length;i++){
+        if(todolist[i].id==id){
+            todolist[i].isComplete= !todolist[i].isComplete
+            break;
+        }
+    }
+    render();
+    console.log(todolist);
+}
+
+function randomIDGenerate(){
+    return '_' + Math.random().toString(36).substr(2, 9);
+}
