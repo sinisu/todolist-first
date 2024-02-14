7ea93fc2c1a9b6dbb6553515fd3482822c8ea393
yisoo song <yisoo1049@gmail.com>
2024-02-15 01:08:23 +0900

    third commit

--- script.js
+++ script.js
@@ -3,6 +3,11 @@ let addButton=document.getElementById("add-button")
 let todolist=[]
 
 addButton.addEventListener("click",addlist)
+userInput.addEventListener("focus",blankfield)
+
+function blankfield(){
+    userInput.value="";
+}
 
 function addlist(){
     let taskItem={
@@ -12,6 +17,7 @@ function addlist(){
     }
     todolist.push(taskItem);
     console.log(todolist);
+    blankfield();
     render();
 }
 
@@ -19,25 +25,26 @@ function render(){
     let resultHTML="";
     for(let i=0;i<todolist.length;i++){
         if(todolist[i].isComplete==true){
-            resultHTML +=`<div class="task-list">
-        <div class="task-done">${todolist[i].taskContent}</div>
-        <div>
-            <button onclick="toggleComplete('${todolist[i].id}')">check</button>
-            <button>delete</button>
-        </div>
-    </div>`
+            resultHTML +=`<div class="task-list done-box">
+                <div class="taskbox1">
+                    <button onclick="toggleComplete('${todolist[i].id}')"><i class="fa-solid fa-plus"></i></button>
+                    <div class="task-done">${todolist[i].taskContent}</div>
+                </div>
+                <div>
+                    <button onclick="deleteTask('${todolist[i].id}')"><i class="fa-solid fa-minus"></i></button>
+                </div>
+            </div>`
         } else{
-            resultHTML +=`<div class="task-list">
-        <div>${todolist[i].taskContent}</div>
-        <div>
-            <button onclick="toggleComplete('${todolist[i].id}')">check</button>
-            <button>delete</button>
-        </div>
+            resultHTML +=`<div class="task-list ready-box">
+            <div class="taskbox1">
+                <button onclick="toggleComplete('${todolist[i].id}')"><i class="fa-solid fa-check"></i></button>
+                <div>${todolist[i].taskContent}</div>
+            </div>
+            <div>
+                <button onclick="deleteTask('${todolist[i].id}')"><i class="fa-solid fa-minus"></i></button>
+            </div>
     </div>`;
         }
-
-        
-
     }
 
     document.getElementById("task-content").innerHTML=resultHTML;
@@ -53,6 +60,18 @@ function toggleComplete(id){
     }
     render();
     console.log(todolist);
+}
+
+function deleteTask(id){
+    console.log("delete",id)
+    for(let i=0;i<todolist.length;i++){
+        if(todolist[i].id==id){
+            todolist.splice(i,1)
+            break;
+        }
+    }
+    render();
+    console.log(todolist)
 }
 
 function randomIDGenerate(){
